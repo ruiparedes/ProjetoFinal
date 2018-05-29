c147968643a13c6abb687290896a6a1793a5a842
ruiparedes <paredesrui16@gmail.com>
2018-05-29 18:04:00 +0100

    Create tables code on NodeJS

--- NodeJSApp/app.js
+++ NodeJSApp/app.js
@@ -55,7 +55,50 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+
+//Database Tables
+
+var UsersTb = "CREATE TABLE if not exists users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20) NOT NULL, password VARCHAR(20) NOT NULL, name VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, address VARCHAR(50) NOT NULL)";
+con.query(UsersTb, function(err, result){
+if(err) throw err;
+console.log("Table Users Created");
 });
+
+var CompetitionsTb = "CREATE TABLE if not exists competitions (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL, totalParticipants INT(2) NOT NULL, numberHelps INT(2) NOT NULL, discountPerHelp INT(2) NOT NULL, estado VARCHAR(15) NOT NULL)";
+con.query(CompetitionsTb, function(err, result){
+if(err) throw err;
+console.log("Table Competitions Created");
+});
+
+var ChallengesTb = "CREATE TABLE if not exists challenges (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL, description VARCHAR(500) NOT NULL, link VARCHAR(100) NOT NULL, solution VARCHAR(100) NOT NULL)";
+con.query(ChallengesTb, function(err, result){
+if(err) throw err;
+console.log("Table Challenges Created");
+});
+
+var ChallengesPerCompetitionTb = "CREATE TABLE if not exists challengesPerCompetition (id INT AUTO_INCREMENT PRIMARY KEY, competitionID INT NOT NULL, challengeID INT NOT NULL, FOREIGN KEY (competitionID) REFERENCES competitions(id), FOREIGN KEY (challengeID) REFERENCES challenges(id), maxScore INT(3) NOT NULL)";
+con.query(ChallengesPerCompetitionTb, function(err, result){
+if(err) throw err;
+console.log("Table ChallengesPerCompetition Created");
+});
+
+var RegistrationsTb = "CREATE TABLE if not exists registrations (id INT AUTO_INCREMENT PRIMARY KEY, userID INT NOT NULL, competitionID INT NOT NULL, FOREIGN KEY (userID) REFERENCES users(id), FOREIGN KEY (competitionID) REFERENCES competitions(id), registrationDate DATETIME NOT NULL)";
+con.query(RegistrationsTb, function(err, result){
+if(err) throw err;
+console.log("Table Registrations Created");
+});
+
+var scorePerChallengePerCompetitionTb = "CREATE TABLE if not exists scorePerChallengePerCompetition (id INT AUTO_INCREMENT PRIMARY KEY, registrationID INT NOT NULL, challengesPerCompetitionID INT NOT NULL, FOREIGN KEY (registrationID) REFERENCES registrations(id), FOREIGN KEY (challengesPerCompetitionID) REFERENCES challengesPerCompetition(id), score INT(3) NOT NULL)";
+con.query(scorePerChallengePerCompetitionTb, function(err, result){
+if(err) throw err;
+console.log("Table scorePerChallengePerCompetition Created");
+});
+
+
+
+
+});
+
 
 
 
