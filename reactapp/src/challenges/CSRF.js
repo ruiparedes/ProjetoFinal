bb26065d03b667446e488b4e5a211c22683946c4
Rui Paredes <paredesrui16@gmail.com>
2018-07-01 16:54:05 +0100

    Por acabar (implementar Route Dinamica)

--- reactapp/src/challenges/CSRF.js
+++ reactapp/src/challenges/CSRF.js
@@ -1,41 +1,64 @@
 import React, { Component } from 'react';
 import { URL } from '../shared/Constants';
+import CSRFProccess from "./CSRFProccessing.js";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
 class CSRF extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            CSRFAttack: ''
+            CSRFAttack: '',
+            redirect: false
         }
-    
+
         this.sendAttack = this.sendAttack.bind(this);
         this.onChange = this.onChange.bind(this);
     }
 
-    
+
     onChange(e) {
         this.setState({ [e.target.className]: e.target.value });
-        console.log({[e.target.className]: e.target.value});
+        console.log({ [e.target.className]: e.target.value });
     }
-    
-sendAttack(){
 
-    //Codigo para enviar os dados para um componente diferente.
+    sendAttack() {
 
-}
+        if(this.state.CSRFAttack){
+            this.setState({redirect: true});
+        }
+
+    }
 
 
     render() {
+
+        if(this.state.redirect == true){
+            console.log(this.state.redirect);
+            this.setState({redirect: false});
+            console.log(this.state.redirect);
+            console.log("Link:"+ this.state.CSRFAttack);
+            return <Redirect
+            to={{
+              pathname: ""+ this.state.CSRFAttack
+            }}
+          />  
+        }
+        
         return (
             <div>
                 <div className="block">
-                        <label>Attack: </label>
-                        <input type="text" placeholder="Enter the Attack" className="CSRFAttack" onChange={this.onChange} required></input>
-                    </div>
+                    <label>Attack: </label>
+                    <input type="text" placeholder="Enter the Attack" className="CSRFAttack" onChange={this.onChange} required></input>
+                </div>
                 <div className="btncontainer" >
                     <button type="submit" value="Attack" className="acceptbtn" onClick={this.sendAttack}>Attack</button>
                 </div>
+                <Router>
+                    <div className="routes-container">
+                            <Route exact path="/bankTransfer" component={CSRFProccess} />
+                    </div>
+                </Router>
             </div>
         );
     }
---
+++ reactapp/src/challenges/CSRFProccessing.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class CSRFProccessing extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+    }
+
+    componentDidMount(){
+
+      
+        
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>herro</h1>
+            </div>
+        );
+    }
+}
+
+export default CSRFProccessing;
